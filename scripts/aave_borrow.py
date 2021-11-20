dc2c815ee49a8fb2fd50a91377841395b955c90b
Sandrino <s.becirbegovic@gmail.com>
2021-11-20 22:04:29 +0100

    approving tx function

--- scripts/aave_borrow.py
+++ scripts/aave_borrow.py
@@ -1,6 +1,9 @@
 from brownie import network, config, interface
 from scripts.get_weth import get_weth
 from scripts.helpful_scripts import get_account
+from web3 import Web3
+
+amount = Web3.toWei(0.1, "ether")
 
 
 def main():
@@ -13,7 +16,18 @@ def main():
     # ABI
     # ADDRESS
     lending_pool = get_lending_pool()
-    print(lending_pool)
+
+    # Approve sending out ERC20 token
+    approve_erc20(amount, lending_pool.address, erc20_address, account)
+
+
+def approve_erc20(amount, spender, erc20_address, account):
+    print("Approving erc20 token...")
+    erc20 = interface.IERC20(erc20_address)
+    tx = erc20.approve(spender, amount, {"from": account})
+    tx.wait(1)
+    print("Approved")
+    return tx
 
 
 def get_lending_pool():
